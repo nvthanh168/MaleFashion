60a993146df6887fe886981bee985cd2083079a3
CunoVox <haovo1512@gmail.com>
2023-03-30 23:59:11 +0700

    update

--- src/main/java/WebProject/WebProject/WebProjectApplication.java
+++ src/main/java/WebProject/WebProject/WebProjectApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 
 import com.cloudinary.Cloudinary;
 
-
 @SpringBootApplication
 public class WebProjectApplication implements CommandLineRunner{
 	@Value("${cloudinary.cloud_name}")
